72ebcf3f8737788c1e0ddf89c543021c560f7e60
Krishna Prasath S <85412620+Krishnaprasath1@users.noreply.github.com>
2025-10-17 23:40:27 +0530

    Update cricketersample.component.ts

--- frontend/src/app/ipl/components/cricketersample/cricketersample.component.ts
+++ frontend/src/app/ipl/components/cricketersample/cricketersample.component.ts
@@ -33,16 +33,5 @@ export class CricketerSampleComponent {
         // }
 
        
-cricketer: Cricketer = new Cricketer(
-  1,
-  1,
-  'Virat',
-  32,
-  'Indian',
-  14,
-  'Batsman',
-  580,
-  50
-);
-
+cricketer = new Cricketer(1,1,"Virat", 35, "Indian",17,"Batsman",18000, 25);  
 }
