ced41325317c655f0d55dc12ea57e5c123dea1da
username <wecp-dummy@gmail.com>
2025-10-19 17:42:33 +0000

    wecp initial commit

--- backend/src/main/java/com/wecp/progressive/jwt/JwtUtil.java
+++ backend/src/main/java/com/wecp/progressive/jwt/JwtUtil.java
@@ -1,5 +1,128 @@
 package com.wecp.progressive.jwt;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+import org.springframework.security.core.userdetails.UserDetails;
+
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+import javax.crypto.SecretKey;
+ 
+
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+ 
+@Component
 public class JwtUtil {
+ 
+    private UserDetailsService userDetailsService;
+    @Autowired
+    public JwtUtil(UserDetailsService userDetailsService){
+            this.userDetailsService=userDetailsService;
+    }
+
+    private final String secret = "secretKey000000000000000000000000000000000000000000000000000000000000000000000000000000000000000";
+    private final int expiration = 1000 * 60 * 60 * 10; // 10 hours
+ 
+    private SecretKey getSigningKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(secret);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+ 
+    public String generateToken(String username) {
+
+    //     Map<String, Object> claims = new HashMap<>();
+    //     claims.put("role", "USER");
+    //    return createToken(claims, username);
+
+     UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+    String role = userDetails.getAuthorities().stream()
+                    .findFirst()
+                    .map(GrantedAuthority::getAuthority)
+                    .orElse("USER");
+
+     
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("role", role);
+        return createToken(claims, username);
+
+    //     Map<String, Object> claims = new HashMap<>();
+    //     claims.put("role", "USER");
+    //    return createToken(claims, username);
+
+    //  UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+    // String role = userDetails.getAuthorities().stream()
+    //                 .findFirst()
+    //                 .map(GrantedAuthority::getAuthority)
+    //                 .orElse("USER");
+
+     
+    //     Map<String, Object> claims = new HashMap<>();
+    //     claims.put("role", role);
+    //     return createToken(claims, username);
+
+    }
+ 
+    private String createToken(Map<String, Object> claims, String subject) {
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(subject)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + expiration))
+                .signWith(getSigningKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+    // String token = generateToken("krishnas");
+    // Claims c = extractAllClaims(token);
+ 
+    public Claims extractAllClaims(String token) {
+        System.out.println("t->"+token);
+        System.out.println(Arrays.hashCode(getSigningKey().getEncoded()));
+        System.out.println("AVLO DHAN");
+
+     //get signing line doubt
+        return Jwts.parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+ 
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+ 
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+ 
+    public boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+ 
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+ 
+    public boolean validateToken(String token, UserDetails userDetails) {
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
 
 }
+ 
+
