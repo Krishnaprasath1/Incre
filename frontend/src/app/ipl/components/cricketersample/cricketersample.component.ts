27967e0bf84c38978a5268208e4f913c00e751f3
username <wecp-dummy@gmail.com>
2025-10-20 09:58:34 +0000

    wecp initial commit

--- frontend/src/app/ipl/components/cricketersample/cricketersample.component.ts
+++ frontend/src/app/ipl/components/cricketersample/cricketersample.component.ts
@@ -1,4 +1,39 @@
+import { Component } from "@angular/core";
+import { Cricketer } from "../../types/Cricketer";
+import { Team } from "../../types/Team";
+
+
+
+@Component({
+  selector: 'app-cricketer-sample',
+ standalone:true,
+  templateUrl: './cricketersample.component.html',
+  styleUrls: ['./cricketersample.component.scss']
+})
+
 
 export class CricketerSampleComponent {
-  
+  team!:Team;
+        // constructor(
+        //   public id: number,
+        //   public name: string,
+        //   public age: number,
+        //   public nationality: string,
+        //   public experience: number,
+        //   public role: string,
+        //   public totalRuns: number,
+        //   public totalWickets: number
+        // ) {
+        //   this.age=age;
+        //   this.experience=experience;
+        //   this.id=id;
+        //   this.name=name;
+        //   this.nationality=nationality;
+        //   this.role=role;
+        //   this.totalRuns=totalRuns;
+        //   this.totalWickets=totalWickets;
+        // }
+
+       
+cricketer = new Cricketer(1,"Virat", 35, "Indian",17,"Batsman",18000, 25,this.team);  
 }
--- frontend/src/app/ipl/components/teamsample/teamsample.component.ts
+++ frontend/src/app/ipl/components/teamsample/teamsample.component.ts
@@ -1,5 +1,28 @@
+import { Component } from "@angular/core";
+import { Team } from "../../types/Team";
 
+@Component({
+  selector: 'app-team-sample',
+  standalone:true,
+  templateUrl: './teamsample.component.html',
+  styleUrls: ['./teamsample.component.scss']
+})
 export class TeamSampleComponent  {
+        // constructor(
+        //   public teamId: number,
+        //   public teamName: string,
+        //   public location: string,
+        //   public ownerName: string,
+        //   public establishmentYear: number
+        // ) {
 
-  
+        //     this.teamId=teamId;
+        //     this.teamName=teamName;
+        //     this.location=location;
+        //     this.ownerName=ownerName;
+        //     this.establishmentYear=establishmentYear;
+        // }
+        
+ team: Team  = new Team(1, "CSK", "Chennai", "Dhoni", 2011);
+
 }
